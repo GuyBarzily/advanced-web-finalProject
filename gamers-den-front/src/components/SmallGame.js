db2e0cdd7d51b20f6a58d7c8f13093fdcff7b112
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2022-12-29 16:27:48 +0200

    fiexed small chages if item

--- gamers-den-front/src/components/SmallGame.js
+++ gamers-den-front/src/components/SmallGame.js
@@ -54,7 +54,7 @@ function SmallGame(props) {
           onClick={handleClick}
         />
         <CardContent>
-          {item.title.length > 20 && (
+          {item && item.title.length > 20 && (
             <Typography
               onClick={handleClick}
               gutterBottom
@@ -65,7 +65,7 @@ function SmallGame(props) {
               {item.title}
             </Typography>
           )}
-          {item.title.length <= 20 && (
+          {item && item.title.length <= 20 && (
             <Typography
               onClick={handleClick}
               gutterBottom
