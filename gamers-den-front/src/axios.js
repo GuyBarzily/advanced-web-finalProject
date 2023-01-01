a253ec7d111a49233c6a189f6b86fea7d3f60eef
roeyrach <93585090+roeyrach@users.noreply.github.com>
2023-01-01 17:52:12 +0200

    getGameCarousel func axios

--- gamers-den-front/src/axios.js
+++ gamers-den-front/src/axios.js
@@ -1,30 +1,35 @@
-import axios from "axios";
+import axios from "axios"
 
 export const getGames = async (genre, platform) => {
-  let data = {};
-  if (genre !== "All") {
-    data.genre = genre;
-  }
-  if (platform !== "All") {
-    data.platform = platform;
-  }
+	let data = {}
+	if (genre !== "All") {
+		data.genre = genre
+	}
+	if (platform !== "All") {
+		data.platform = platform
+	}
 
-  const games = await axios.post("http://localhost:8080/getGames", data);
-  return games.data;
-};
+	const games = await axios.post("http://localhost:8080/getGames", data)
+	return games.data
+}
 
 export const gameByName = async (name) => {
-  const games = await axios.post("http://localhost:8080/gameName", name);
-  return games.data;
-};
+	const games = await axios.post("http://localhost:8080/gameName", name)
+	return games.data
+}
 
 export const addUser = async (data) => {
-  const user = await axios.post("http://localhost:8080/addUser", data);
-  return user.data;
-};
+	const user = await axios.post("http://localhost:8080/addUser", data)
+	return user.data
+}
 
 export const getUser = async (userId) => {
-  const data = { _id: userId };
-  const user = await axios.post("http://localhost:8080/getUser", data);
-  return user.data;
-};
+	const data = { _id: userId }
+	const user = await axios.post("http://localhost:8080/getUser", data)
+	return user.data
+}
+
+export const getCarouselGames = async () => {
+	const games = await axios.post("http://localhost:8080/Carousel")
+	return games.data
+}
--- gamers-den-front/src/components/JoyCard.js
+++ gamers-den-front/src/components/JoyCard.js
@@ -13,60 +13,53 @@ export default function JoyCard(props) {
 
 	useEffect(() => {
 		setGame(props.game)
-		console.log(game)
+		console.log(props.game)
 	}, [props.game])
 
 	const h1Style = {
 		color: "yellow",
 		padding: "5px",
 	}
-	const g = {
-		id: 516,
-		title: "PUBG: BATTLEGROUNDS",
-		thumbnail: "https://www.freetogame.com/g/516/thumbnail.jpg",
-		short_description:
-			"Get into the action in one of the longest running battle royale games PUBG Battlegrounds.",
-		game_url: "https://www.freetogame.com/open/pubg",
-		genre: "Shooter",
-		platform: "PC (Windows)",
-		publisher: "KRAFTON, Inc.",
-		developer: "KRAFTON, Inc.",
-		release_date: "2022-01-12",
-		freetogame_profile_url: "https://www.freetogame.com/pubg",
+	let url
+	if (game) {
+		url = "url(" + game.thumbnail + ")"
 	}
-	const url = "url(" + g.thumbnail + ")"
 	return (
-		<Box
-			sx={{
-				display: "flex",
-				alignItems: "flex-end",
-				flexDirection: "column",
-				width: "50vw",
-				position: "block",
-			}}
-		>
-			<Box
-				sx={{
-					width: "inherit",
-					height: "50vh",
-					backgroundImage: url,
-					backgroundRepeat: "no-repeat",
-					backgroundSize: "contain",
-				}}
-			></Box>
-			<Box
-				sx={{
-					width: "inherit",
-					background:
-						"linear-gradient(90deg, rgba(32,32,32,1) 0%, rgba(40,40,59,1) 61%, rgba(32,32,32,1) 100%);",
-					display: "flex",
-					wrap: "flex-wrap",
-					flexDirection: "column",
-				}}
-			>
-				<h2 style={h1Style}>{g.title}</h2>
-				<h4 style={h1Style}>{g.short_description}</h4>
-			</Box>
-		</Box>
+		<>
+			{game && (
+				<Box
+					sx={{
+						display: "flex",
+						alignItems: "flex-end",
+						flexDirection: "column",
+						width: "50vw",
+						position: "block",
+					}}
+				>
+					<Box
+						sx={{
+							width: "inherit",
+							height: "50vh",
+							backgroundImage: url,
+							backgroundRepeat: "no-repeat",
+							backgroundSize: "contain",
+						}}
+					></Box>
+					<Box
+						sx={{
+							width: "inherit",
+							background:
+								"linear-gradient(90deg, rgba(32,32,32,1) 0%, rgba(40,40,59,1) 61%, rgba(32,32,32,1) 100%);",
+							display: "flex",
+							wrap: "flex-wrap",
+							flexDirection: "column",
+						}}
+					>
+						<h2 style={h1Style}>{game.title}</h2>
+						<h4 style={h1Style}>{game.short_description}</h4>
+					</Box>
+				</Box>
+			)}
+		</>
 	)
 }
