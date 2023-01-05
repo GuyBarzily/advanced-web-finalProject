631416de05c79d108d0879e872eb1ee500242fd0
roeyrach <93585090+roeyrach@users.noreply.github.com>
2023-01-05 13:11:34 +0200

    websockets front

--- gamers-den-front/src/pages/Admin.js
+++ gamers-den-front/src/pages/Admin.js
@@ -2,6 +2,7 @@ import React from "react"
 import AddGame from "../components/AddGame.js"
 import EditUser from "../components/EditUser.js"
 import DeleteGame from "../components/DeleteGame.js"
+import SimpleBarChart from "../components/SimpleBarChart.js"
 
 function Admin(props) {
 	return (
@@ -9,6 +10,9 @@ function Admin(props) {
 			<EditUser></EditUser>
 			<AddGame></AddGame>
 			<DeleteGame></DeleteGame>
+			<div>
+				<SimpleBarChart></SimpleBarChart>
+			</div>
 		</>
 	)
 }
